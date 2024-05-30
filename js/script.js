b872c127a6497d8c3bf96aaa2432677a77e1134f
learning2ProgramNF <nick.finn6@gmail.com>
2024-05-30 14:16:31 -0600

    Update script.js
    added 2 new objects, with 3 keys each.
    Ninetales & Muk.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let pokemonlist = [
     {name: 'Bulbasaur', height: .7, type: ['grass','posion'] },
     {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-    {name: 'Sandslash', height: 1, type: 'ground'}
+    {name: 'Sandslash', height: 1, type: 'ground'},
+    {name: 'Ninetales', height: 1.1, type: 'fire'},
+    {name: 'Muk', height: 1.2, type: 'posion'}
 ];
