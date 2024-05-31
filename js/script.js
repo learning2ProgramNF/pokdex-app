9317bf37072661da0b16a4a481a635f71276cb5c
learning2ProgramNF <nick.finn6@gmail.com>
2024-05-31 14:20:53 -0600

    Added function to replace hardcoded function
    cleaned up js, added function that is able to take any list parameter and print it to the DOM as well as check for big Pokemon.
    Removed unused code

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
-let pokemonlist = [
-    {name: 'Bulbasaur', height: .7, type: ['grass','posion'] },
-    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-    {name: 'Sandslash', height: 1, type: 'ground'},
-    {name: 'Ninetales', height: 1.1, type: 'fire'},
-    {name: 'Muk', height: 1.2, type: 'posion'}
+/*
+'array' 'declared' and named pokemonList
+5 'objects' are in the array with 3 'properties' each
+*/
+let pokemonList = [
+    { name: 'Bulbasaur', height: .7, type: ['grass', 'posion'] },
+    { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] },
+    { name: 'Sandslash', height: 1, type: 'ground' },
+    { name: 'Ninetales', height: 1.1, type: 'fire' },
+    { name: 'Muk', height: 1.2, type: 'posion' }
 ];
 
-let text = "";
+/*
+'for loop' created in order to 'iterate' through each of the 'objects' in the 'Array'.
+1 at a time and print them to the 'DOM' as well as 'conditionally' adding a special message,
+when they are of a certain size.
 
+'function' created to re-use thise bit of code that print the array multiple times instead of typing it out. 
+*/
 
-for (let i =0;pokemonlist[i];i++) {
-    document.write(pokemonlist[i].name + ' ');
-    document.write(' Height: ' + pokemonlist[i].height + ' ');
-    if (pokemonlist[i].height > 1.1){
-        document.write('\n'+' Woah that\'s a big pokemon ');
+function printArrayDetails(list){
+    for (let i = 0;list.length; i++) {
+        document.write(list[i].name + ' ');
+        document.write(' Height: ' + list[i].height);
+        if (list[i].height > 1.1) {
+            document.write(' Wow that\'s a big Pokemon ' + '<p>');
+        } else {
+            document.write('<p>');
+        }
     }
-}
-    // } else if (pokemonlist[i].height < 1.1 && pokemonlist[i].height > .8) {
-    //     document.write(' Woah that\'s a average pokemon ');
-    // } else {
-    //     document.write(' Woah that\'s a small pokemon');
-    // Realized later I was only suposed to highlight special pokemon. 
+}   
+
+//'Calls' to the 'function' that prints out the 'array'
+printArrayDetails(pokemonList);
+printArrayDetails(pokemonList);
+
+
+
