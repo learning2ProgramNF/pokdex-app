951bbb1bf9ed04f41a7f7882973d2cef3e314b96
learning2ProgramNF <nick.finn6@gmail.com>
2024-06-03 13:46:57 -0600

    pokemonlist2 array added
    added second array
    called to function to print second array just like the first array.
    added i < list[i].length to be able to re-use the function there was an error that was occurring when I was using the function twice.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,19 @@
 5 'objects' are in the array with 3 'properties' each
 */
 let pokemonList = [
-    { name: 'Bulbasaur', height: .7, type: ['grass', 'posion'] },
-    { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] },
-    { name: 'Sandslash', height: 1, type: 'ground' },
-    { name: 'Ninetales', height: 1.1, type: 'fire' },
-    { name: 'Muk', height: 1.2, type: 'posion' }
+  { name: "Bulbasaur", height: 0.7, type: ["grass", "posion"] },
+  { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
+  { name: "Sandslash", height: 1, type: ["ground"] },
+  { name: "Ninetales", height: 1.1, type: ["fire"] },
+  { name: "Muk", height: 1.2, type: ["posion"] },
 ];
-
+let pokemonList2 = [
+  { name: "Magmar", height: 1.3, type: ["fire"] },
+  { name: "Gyarados", height: 6.5, type: ["water", "flying"] },
+  { name: "Dragonite", height: 2.2, type: ["dragon", "flying"] },
+  { name: "Diglett", height: 0.2, type: ["ground"] },
+  { name: "Porygon", height: 0.8, type: ["normal"] },
+];
 /*
 'for loop' created in order to 'iterate' through each of the 'objects' in the 'Array'.
 1 at a time and print them to the 'DOM' as well as 'conditionally' adding a special message,
@@ -18,21 +24,19 @@ when they are of a certain size.
 'function' created to re-use thise bit of code that print the array multiple times instead of typing it out. 
 */
 
-function printArrayDetails(list){
-    for (let i = 0;list.length; i++) {
-        document.write(list[i].name + ' ');
-        document.write(' Height: ' + list[i].height);
-        if (list[i].height > 1.1) {
-            document.write(' Wow that\'s a big Pokemon ' + '<p>');
-        } else {
-            document.write('<p>');
-        }
+function printArrayDetails(list) {
+  for (let i = 0; i < list.length; i++) {
+    document.write("<p>" + list[i].name + " ");
+    document.write(" Height: " + list[i].height);
+    if (list[i].height > 1.1) {
+      document.write(" Wow that's a big Pokemon " + "<p>");
+    } else {
+      document.write("<p>");
     }
-}   
+  }
+}
 
 //'Calls' to the 'function' that prints out the 'array'
 printArrayDetails(pokemonList);
-printArrayDetails(pokemonList);
 
-
-
+printArrayDetails(pokemonList2);
