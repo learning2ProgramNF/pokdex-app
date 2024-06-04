7ec25a46afabbd6f2c8e6bcce828b2eaecf1500c
learning2ProgramNF <nick.finn6@gmail.com>
2024-06-04 15:27:26 -0600

    Add code - 6/4/24 3:25PM
    forEach Loop added
    IIFE wrapped around pokemonList
    Functions 'add' and 'getAll' declared within IIFE with code to add single item to pokemonList and get copy of the current array with 'getAll'.
    Then we printed to the DOM and also added conditional to check if the pokemon was big or not

--- js/script.js
+++ js/script.js
@@ -2,34 +2,43 @@
 'array' 'declared' and named pokemonList
 5 'objects' are in the array with 3 'properties' each
 */
-let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, type: ["grass", "posion"] },
-  { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
-  { name: "Sandslash", height: 1, type: ["ground"] },
-  { name: "Ninetales", height: 1.1, type: ["fire"] },
-  { name: "Muk", height: 1.2, type: ["posion"] },
-];
-let pokemonList2 = [
-  { name: "Magmar", height: 1.3, type: ["fire"] },
-  { name: "Gyarados", height: 6.5, type: ["water", "flying"] },
-  { name: "Dragonite", height: 2.2, type: ["dragon", "flying"] },
-  { name: "Diglett", height: 0.2, type: ["ground"] },
-  { name: "Porygon", height: 0.8, type: ["normal"] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, type: ["grass", " posion"] },
+    { name: "Charizard", height: 1.7, type: ["fire", " flying"] },
+    { name: "Sandslash", height: 1, type: ["ground"] },
+    { name: "Ninetales", height: 1.1, type: [" fire"] },
+    { name: "Muk", height: 1.2, type: ["posion"] },
+    { name: "Magmar", height: 1.3, type: ["fire"] },
+    { name: "Gyarados", height: 6.5, type: ["water", " flying"] },
+    { name: "Dragonite", height: 2.2, type: ["dragon", " flying"] },
+    { name: "Diglett", height: 0.2, type: [" ground"] },
+    { name: "Porygon", height: 0.8, type: [" normal"] },
+  ];
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
+
 /*
-'for loop' created in order to 'iterate' through each of the 'objects' in the 'Array'.
+'forEach loop' created in order to 'iterate' through each of the 'objects' in the 'Array'.
 1 at a time and print them to the 'DOM' as well as 'conditionally' adding a special message,
 when they are of a certain size.
-
-'function' created to re-use thise bit of code that print the array multiple times instead of typing it out. 
 */
-//forEach loop to replace above with more effiecient functionality. 
-pokemonList.forEach(function (pokemon) {
-  document.write('<p>' + pokemon.name + ' ' +  pokemon.height + ' ' + pokemon.type + '<p>');
 
+// forEach loop to replace above with more effiecient functionality.
+pokemonRepository.getAll().forEach(function (pokemon) {
+  document.write(
+    "<p>" + pokemon.name + " " + pokemon.height + " " + pokemon.type
+  );
+  if (pokemon.height > 1.1) {
+    document.write(" - Wow!!! that's a big Pokemon  " + "<p>");
+  }
 });
-pokemonList2.forEach(function (pokemon) {
-  document.write('<p>' + pokemon.name + ' ' +  pokemon.height + ' ' + pokemon.type + '<p>');
-
-});
-
